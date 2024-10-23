261e251828b5f1547eae30d1157b1a1a3a745006
Shekhina OGOU <bidossessiaiglineshekhina.ogou@monboreal.ca>
2024-10-23 11:28:41 -0400

    Creation de la classe fille CarteCredit et de ses instances dans Program.cs

---
+++ AppPaiementss/CarteCredit.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiementss
+{
+    internal class CarteCredit: Paiement
+    {
+        public int NumeroCarte { get; set; }
+
+        public CarteCredit(double montant, string description, int carte) : base(montant, description)
+        {
+
+            NumeroCarte = carte;
+
+        }
+
+        public override void AfficherDetails()
+        {
+            Console.WriteLine($"La description du paiement est {Description}, il a une valeur de {Montant}$ et il provient de la carte {NumeroCarte}.");
+        }
+
+
+
+    }
+}
--- AppPaiementss/Program.cs
+++ AppPaiementss/Program.cs
@@ -1,7 +1,16 @@
-﻿internal class Program
+﻿using AppPaiementss;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        CarteCredit carte1 = new CarteCredit(14.68, "Le Beau", 509089123);
+        CarteCredit carte2 = new CarteCredit(20.99, "Canada WonderLand", 618119893);
+        CarteCredit carte3 = new CarteCredit(80.56, "No Frills", 763248654);
+
+
+        carte1.AfficherDetails();
+        carte2.AfficherDetails();
+        carte3.AfficherDetails();
     }
 }
