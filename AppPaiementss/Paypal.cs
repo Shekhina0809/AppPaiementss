85a38a8fbf46e2c7fbaba90a1c8e9cd470e38c90
Shekhina OGOU <bidossessiaiglineshekhina.ogou@monboreal.ca>
2024-10-23 11:49:09 -0400

    Creation de la classe fille Paypal

---
+++ AppPaiementss/Paypal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiementss
+{
+    internal class Paypal: Paiement
+    {
+        public string Courriel { get; set; }
+
+        public Paypal(double montant, string description, string email) : base(montant, description)
+        {
+
+            Courriel = email;
+
+        }
+
+        public override void AfficherDetails()
+        {
+            Console.WriteLine($"La description du paiement est {Description}, il a une valeur de {Montant}$ et l'email est {Courriel}.");
+        }
+
+    }
+}
