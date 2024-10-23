67380b39a6add9be618fb2330b43cf3883cb56cb
Shekhina OGOU <bidossessiaiglineshekhina.ogou@monboreal.ca>
2024-10-23 11:06:12 -0400

    Ajout de la classe abstraite Paiement

---
+++ AppPaiementss/Paiement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiementss
+{
+    internal class Paiement
+    {
+        public double Montant { get; set; }
+        public string Description { get; set; }
+
+
+
+        public Paiement(double montant, string description)
+        {
+            Montant = montant;
+            Description = description;
+
+        }
+
+        public virtual void AfficherDetails()
+        {
+            Console.WriteLine($"La description du paiement est: {Description} et il a une valeur de {Montant} $");
+        }
+
+    }
+}
